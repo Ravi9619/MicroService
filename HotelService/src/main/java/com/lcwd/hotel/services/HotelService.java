da74eebf47b73e0c10ad99f1dfb1af3910d37025
Ravi <dubeyravi9619@gmail.com>
2023-03-19 12:26:12 +0530

    Hotel Service

---
+++ HotelService/src/main/java/com/lcwd/hotel/services/HotelService.java
@@ -0,0 +1,16 @@
+package com.lcwd.hotel.services;
+
+import com.lcwd.hotel.entities.Hotel;
+
+import java.util.List;
+
+public interface HotelService {
+
+    //create
+    Hotel create(Hotel hotel);
+
+    //getall
+    List<Hotel> getAll();
+
+    Hotel get(String id);
+}
